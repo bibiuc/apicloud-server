607b4b66bd147af305f67b90c28eec75b0045af4
bbuc <bbuc@qq.com>
2017-04-24 11:09:43 +0800

    test update

--- test/index.js
+++ test/index.js
@@ -4,13 +4,14 @@ var WebSocket = require('ws');
 var server = new Server({
   projects: ['/Users/xukaixuan/Projects/taxi/passenger-gulp-app/*', '/Users/xukaixuan/Projects/taxi/hx-ser/svn/*']
 });
-// setTimeout(function () {
-//   var ws = new WebSocket('ws://127.0.0.1:' + server.info().port);
-//   ws.on('open', function() {
-//     ws.send(JSON.stringify({
-//       command: 2,
-//       appid: 'A6932196309087',
-//       timestamp: new Date().getTime() / 1000
-//     }));
-//   });
-// }, 2000);
+server.onLog(console.log);
+setTimeout(function () {
+  var ws = new WebSocket('ws://127.0.0.1:' + server.info().port);
+  ws.on('open', function() {
+    ws.send(JSON.stringify({
+      command: 2,
+      appid: 'A6932196309087',
+      timestamp: new Date().getTime() / 1000
+    }));
+  });
+}, 2000);
